3ff5716a5514e50fc987a72d3d6ca779140d47b8
khamel <khamel@ucalgary.ca>
2018-04-08 17:21:13 -0600

    Counting of other types

--- Source/TypeTracker.java
+++ Source/TypeTracker.java
@@ -3,4 +3,5 @@ public class TypeTracker {
 	public static int numberOfNested = 0;
 	public static int numberOfLocal = 0;
 	public static int numberOfAnonymous = 0;
+	public static int numberOfOther = 0;
 }
